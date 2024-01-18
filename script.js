a2ce44bf3b9f1792a86ff416d2909e24819fd9e8
Göran Olson <goran@famolson.se>
2024-01-18 14:07:46 +0100

    added exercises 4 - 6

--- script.js
+++ script.js
@@ -18,3 +18,33 @@ for (let i = 0; i < nbrs.length; i++) {
     }
 }
 console.log(newArray);
+
+
+/* 4 */
+const numberSelectable = [3,2,1,4];
+let sum = 0;
+
+for (let i = 0; i < numberSelectable.length; i++) {
+    sum += numberSelectable[i];   
+}
+console.log(sum);
+
+
+/* 5 */
+const names = ['Adam', 'Bertil', 'Carl', 'David', 'Erik'];
+
+for (const name of names) {
+    if(name === 'Bertil') {
+        console.log(name);
+        break;
+    }
+}
+
+
+/* 6 */
+const cars = ['Volvo', 'Saab', 'BMW'];
+
+cars.splice(-1);
+
+console.log(cars);
+
